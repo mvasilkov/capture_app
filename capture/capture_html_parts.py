362fe35a738563fffa381e3dbf55283309540920
Mark Vasilkov <mvasilkov@gmail.com>
2019-01-02 23:35:50 +0200

    Ignore duplicates (because --anticache)

--- capture/capture_html_parts.py
+++ capture/capture_html_parts.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 from pathlib import Path, PurePath
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urlunparse
 
 from mitmproxy import http
 
@@ -19,6 +19,10 @@ def good_type(a: str):
     return a == 'text/html' or a.startswith('image/')
 
 
+def get_canon_addr(a: str):
+    return urlunparse(urlparse(a))
+
+
 def get_name(a: str):
     return PurePath(urlparse(a).path).name
 
@@ -32,14 +36,19 @@ class CaptureParts(Capture):
         super().__init__()
 
         self.counters = defaultdict(int)
+        self.captured_pages = set()
 
     def should_capture(self, flow: http.HTTPFlow):
         content_type = flow.response.headers.get('content-type', 'unknown content type')
-        if good_addr(flow.request.url) and good_type(content_type):
-            if content_type != 'text/html':
-                return get_name(flow.request.url)
+        canon_addr = get_canon_addr(flow.request.url)
+        if (good_addr(canon_addr) and good_type(content_type)
+                and canon_addr not in self.captured_pages):
+            self.captured_pages.add(canon_addr)
 
-            key = get_key(flow.request.url)
+            if content_type != 'text/html':
+                return get_name(canon_addr)
+
+            key = get_key(canon_addr)
             self.counters[key] += 1
 
             assert 'referer' in flow.request.headers
